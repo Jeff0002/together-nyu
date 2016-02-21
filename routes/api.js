a336b0710d5b6775a8cb09704e47f03cd3d09d5d
Tebyt <chenpan5532323@gmail.com>
2016-02-20 20:41:35 -0500

    get activity

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var mongodb = require('mongodb');
 
 
-var activitySchema = new mongoose.Schema( {
+var activitySchema = new mongoose.Schema({
     userid: String,
     type: String,
-    loc: { type: [Number], index: '2dsphere' },
+    loc: {
+        type: [Number],
+        index: '2dsphere'
+    },
     subject: String,
     description: String,
     startTime: Date,
@@ -15,7 +19,7 @@ var activitySchema = new mongoose.Schema( {
 
 var activity = mongoose.model('activity', activitySchema);
 
-var userSchema = new mongoose.Schema( {
+var userSchema = new mongoose.Schema({
     username: String,
     password: String
 })
@@ -23,57 +27,96 @@ var userSchema = new mongoose.Schema( {
 var user = mongoose.model('user', userSchema);
 
 
-router.post('/user', function(req, res, next) {
-    user.create(req.body, function(err, data) {
+router.post('/user', function (req, res, next) {
+    user.create(req.body, function (err, data) {
         if (err) return next(err);
         res.json(data);
-    }) 
+    })
 });
 
-router.get('/user/:username/:password', function(req, res, next) {
-    user.find({username: req.params.username}, function(err, data) {
+router.get('/user/:username/:password', function (req, res, next) {
+    user.find({
+        username: req.params.username
+    }, function (err, data) {
         console.log(req.params.password);
         console.log(data.password);
         if (err) return next(err);
         if (data.password != req.params.password) return res.sendStatus(401);
-        activity.find({userid: req.params.username}, function(err, data) {
+        activity.find({
+            userid: req.params.username
+        }, function (err, data) {
             if (err) return next(err);
             res.json(data);
         })
     })
-    
+
 })
 
-router.get('/activity/:loc', function(req, res) {
-    var locParam = JSON.parse(req.params.loc);
+router.get('/activity/:loc', function (req, res) {
+    var locParam = JSON.parse(req.params.loc).data;
     console.log(locParam);
+
     
-    activity.find({
-        "loc": {
-            $near: {
-                $geometry: {
-                    "type": "Point",
-                    "coordinates": [locParam[0], locParam[1]]
-                },
-                $maxDistance: locParam[2]
-            }
-        }
-    }, function(err, data) {
-        if (err) res.send(err);
-      res.json(data);
-    })
-    
+    mongodb.MongoClient.connect('mongodb://tebyt:togethernyu@ds013738.mongolab.com:13738/heroku_t1qnbv72', function (err, db) {
+        if (err) throw err;
+        db.collection('activities')
+            .find({
+                loc: {
+                    $geoWithin: {
+                        $box: locParam
+                    }
+                }
+            })
+            .toArray(function (err, results) {
+//                console.log(results);
+//                if (!results)
+//                    results = [];
+
+                res.json(results);
+
+                db.close();
+            });
+    });
+
+
+
+    //    activity.find({
+    //        "loc": {
+    //            $near: {
+    //                $geometry: {
+    //                    "type": "Point",
+    //                    "coordinates": [locParam[0], locParam[1]]
+    //                },
+    //                $maxDistance: locParam[2]
+    //            }
+    //        }
+    //    }, function(err, data) {
+    //        if (err) res.send(err);
+    //      res.json(data);
+    //    })
+
 });
 
 router.post('/activity', function (req, res, next) {
-    activity.create(req.body, function(err, data) {
+    activity.create(req.body, function (err, data) {
         if (err) return next(err);
         res.json(data);
-    })  
+    })
 });
 
-router.get('/hello', function(req, res) {
-    res.json({"test": "hello"});
+
+
+router.get('/hello', function (req, res) {
+    res.json({
+        "test": "hello"
+    });
 })
+
+router.get('/activity-all', function (req, res, next) {
+    activity.find(function (err, data) {
+        if (err) return next(err);
+        res.json(data);
+    });
+});
 
 module.exports = router;
